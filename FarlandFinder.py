c820f4efd87b5459e905d9608fca6fb019afd628
MSte2 <MSte2@DESKTOP-1KK2DSB>
2024-08-27 16:31:55 +0800

    添加项目文件。

---
+++ FarlandFinder.py
@@ -0,0 +1,14 @@
+﻿import copy
+from turtle import pos
+MinNum=0
+MaxNum=0
+#初始化程序
+MinNum=int(input("最小边界值:"))
+MaxNum=int(input("最大边界值:"))
+while True:
+    Posi=(MinNum+MaxNum)//2#计算坐标值
+    BorS=input(f"{Posi}，大(b)还是小(s)?:")
+    if BorS == "b":
+        MaxNum=copy.deepcopy(Posi)
+    else:
+        MinNum=copy.deepcopy(Posi)
